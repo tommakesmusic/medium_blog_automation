cb0e5677868145f16af9a60b021641f0b975682e
thomaswatsonv1 <95492776+thomaswatsonv1@users.noreply.github.com>
2022-01-28 11:55:26 +0000

    Remove unused params

--- Azure-MI-VM-Control.ps1
+++ Azure-MI-VM-Control.ps1
@@ -1,28 +1,10 @@
 Param(
- [string]$automationaccount,
- [string]$resourcegroup,
  [string]$vmname,
- [string]$method,
- [string]$mi_client_id,
  [string]$mi_principal_id
 )
 
 # Ensures you do not inherit an AzContext in your runbook
 Disable-AzContextAutosave -Scope Process | Out-Null
-
-#Get-AzUserAssignedIdentity -ResourceGroupName $resourcegroup -Name $mi_principal_id).PrincipalId
-#$UAMI = (Get-AzUserAssignedIdentity -ResourceGroupName $resourceGroup -Name $userAssignedManagedIdentity).PrincipalId
-
-
-# Connect using a Managed Service Identity
-# try {
-#         $AzureContext = (Connect-AzAccount -Identity).context
-#     }
-# catch{
-#         Write-Output "There is no system-assigned user identity. Aborting.";
-#        exit
-#    }
-
 
 # Connect to Azure with user-assigned managed identity
 $AzureContext = (Connect-AzAccount -Identity -AccountId $mi_principal_id).context
@@ -31,40 +13,7 @@ $AzureContext = (Connect-AzAccount -Identity -AccountId $mi_principal_id).contex
 $AzureContext = Set-AzContext -SubscriptionName $AzureContext.Subscription -DefaultProfile $AzureContext
 
 Write-Output "Account ID of current context: " $AzureContext.Account.Id
-<#
-if ($method -eq "sa")
-    {
-        Write-Output "Using system-assigned managed identity"
-    }
-elseif ($method -eq "ua")
-    {
-        Write-Output "Using user-assigned managed identity"
 
-        # Connects using the Managed Service Identity of the named user-assigned managed identity
-        $identity = Get-AzUserAssignedIdentity -ResourceGroupName $resourcegroup `
-            -Name $mi_principal_id
-        
-            -DefaultProfile $AzureContext
-
-        # validates assignment only, not perms
-        if ((Get-AzAutomationAccount -ResourceGroupName $resourcegroup `
-                -Name $automationaccount `
-                -DefaultProfile $AzureContext).Identity.UserAssignedIdentities.Values.PrincipalId.Contains($identity.PrincipalId))
-            {
-                $AzureContext = (Connect-AzAccount -Identity -AccountId $identity.ClientId).context
-
-                # set and store context
-           }
-        else {
-                Write-Output "Invalid or unassigned user-assigned managed identity"
-                exit
-            }
-    }
-else {
-        Write-Output "Invalid method. Choose us or sa."
-        exit
-     }
-#>
 # Get current state of VM
 $status = (Get-AzVM -ResourceGroupName $resourcegroup -Name $vmname -Status -DefaultProfile $AzureContext).Statuses[1].Code
 
@@ -84,4 +33,3 @@ elseif ($status -eq "Powerstate/running")
 $status = (Get-AzVM -ResourceGroupName $resourcegroup -Name $vmname -Status -DefaultProfile $AzureContext).Statuses[1].Code  
 
 Write-Output "`r`n Ending VM status: $status `r`n `r`n"
-
