0ce365206e488fc014541f90f74778ef1f16aa8f
thomaswatsonv1 <95492776+thomaswatsonv1@users.noreply.github.com>
2022-01-28 15:17:06 +0000

    Updated to use the principal_id

--- Stop-Start-AzureVM-update.ps1
+++ Stop-Start-AzureVM-update.ps1
@@ -4,35 +4,39 @@ Param
 (    
     [Parameter(Mandatory=$true)][ValidateNotNullOrEmpty()] 
     [String] 
-    $Client_Id, 
+    $resourcegroup,    
     [Parameter(Mandatory=$true)][ValidateNotNullOrEmpty()] 
     [String] 
-    $AzureVMList, 
+    $mi_principal_id, 
+    [Parameter(Mandatory=$true)][ValidateNotNullOrEmpty()] 
+    [String] 
+    $vmlist, 
     [Parameter(Mandatory=$true)][ValidateSet("Start","Stop")] 
     [String] 
-    $Action 
+    $action 
 )
 
 # Ensures you do not inherit an AzContext in your runbook
-Disable-AzContextAutosave -Scope Process
+Disable-AzContextAutosave -Scope Process | Out-Null
 
-# Connect to Azure with system-assigned managed identity - from deployment
-$AzureContext = (Connect-AzAccount -Identity -AccountId $Client_Id).context
-
-# To use this runbook with a system assigned managed identity, replace the above with
-# $AzureContext = (Connect-AzAccount -Identity).context
+# Connect to Azure with user-assigned managed identity
+# Don't do what Microsoft say - they use client_id here but it needs to be
+# the managed_identity_principal_id
+$AzureContext = (Connect-AzAccount -Identity -AccountId $mi_principal_id).context
 
 # set and store context
 $AzureContext = Set-AzContext -SubscriptionName $AzureContext.Subscription -DefaultProfile $AzureContext
+
+Write-Output "Account ID of current context: " $AzureContext.Account.Id
 
 # Get all VM names from the subscription
 Get-AzVM -DefaultProfile $AzureContext | Select-Object Name
 
 
 
-if($AzureVMList -ne "All") 
+if($vmlist -ne "All") 
 { 
-    $AzureVMs = $AzureVMList.Split(",") 
+    $AzureVMs = $azurevmlist.Split(",") 
     [System.Collections.ArrayList]$AzureVMsToHandle = $AzureVMs 
 } 
 else 
@@ -47,10 +51,14 @@ foreach($AzureVM in $AzureVMsToHandle)
     if(!(Get-AzureRmVM | Where-Object {$_.Name -eq $AzureVM})) 
     { 
         throw " AzureVM : [$AzureVM] - Does not exist! - Check your inputs " 
-    } 
+    } else {
+        # Get current state of VM
+        $status = (Get-AzVM -ResourceGroupName $resourcegroup -Name $AzureVM -Status -DefaultProfile $AzureContext).Statuses[1].Code
+        Write-Output "`r`n Beginning VM status: $status `r`n"   
+    }
 } 
 
-if($Action -eq "Stop") 
+if($action -eq "Stop") 
 { 
     Write-Output "Stopping VMs";
     $workflow = $_
