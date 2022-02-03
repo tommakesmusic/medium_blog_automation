78d7e2801fa7fe641491f2a5b021db7d5bdee6b2
thomaswatsonv1 <95492776+thomaswatsonv1@users.noreply.github.com>
2022-02-03 08:53:28 +0000

    Update new-MI-VM-Control.ps1

--- new-MI-VM-Control.ps1
+++ new-MI-VM-Control.ps1
@@ -1,5 +1,30 @@
-# This is an amalgamation of several different runbooks on GitHub
+<#
+.SYNOPSIS
+    Control Azure VMs with an Automation Account and a User-assigned Managed Identity.
+.DESCRIPTION
+    Allows control over a list of Azure Virtual Machines. Runbook can Start, Stop and
+    Read the status of each Virtual Machine in a list pass in as a parameter.
 
+.NOTES
+  Version:        1.0.1
+  Author:         Thomas Watson
+  Creation Date:  2022-01-18
+  GitHub:         
+
+Based on existing Runbooks by Pradebban Raja and Andreas Dieckmann as well as my own additions
+
+.INPUTS
+    Azure resource group (String)
+    User-assigned Managed Identity principal_id (String)
+    List of Virtual Machines to be controlled (string array)
+    Action - either 'Start' or 'Stop' to control VMs 
+
+.OUTPUTS
+    Outputs the Account Id of the current context
+    the start and end times of the script, as well
+    as the beginning and ending status of each VM (along with errors).
+
+#>
 
 Param(
     [Parameter(Mandatory=$true)][ValidateNotNullOrEmpty()] 
@@ -8,13 +33,14 @@ Param(
     [Parameter(Mandatory=$true)][ValidateNotNullOrEmpty()] 
     [String] 
     $mi_principal_id, 
-    [Parameter(Mandatory=$true)][ValidateNotNullOrEmpty()] 
-    [String] 
+    [parameter(Mandatory=$true)][ValidateNotNullOrEmpty()]
+	[string[]]
     $vmlist, 
     [Parameter(Mandatory=$true)][ValidateSet("Start","Stop")] 
     [String] 
     $action
 )
+
 
 Write-Output "Script started at $(Get-Date -Format 'yyyy-MM-dd HH:mm:ss')"
 # Ensures you do not inherit an AzContext in your runbook
@@ -30,13 +56,13 @@ $AzureContext = Set-AzContext -SubscriptionName $AzureContext.Subscription -Defa
 
 Write-Output "Account ID of current context: " $AzureContext.Account.Id
 
-# Separate our vmlist into an arraay we can iterate over
-$AzureVMs = $vmlist.Split(",") 
-[System.Collections.ArrayList]$VMsToChange = $AzureVMs 
+# Separate our vmlist into an array we can iterate over
+$VMssplit = $vmlist.Split(",") 
+[System.Collections.ArrayList]$VMs = $VMssplit
 
 # Loop through one or more VMs which will be passed in from the terraform as a list
 # If the list is empty it will skip the block
-foreach ($VM in $VMsToChange) {
+foreach ($VM in $VMs) {
 
     $status = (Get-AzVM -ResourceGroupName $resourcegroup -Name $VM -Status -DefaultProfile $AzureContext).Statuses[1].Code
     Write-Output "`r`n Initial $VM VM status: $status `r`n `r`n"
